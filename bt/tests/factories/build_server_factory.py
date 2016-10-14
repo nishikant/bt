c19e0ab1490428929f75971995f48f24acc38318
gattu <gattu@questionpro.com>
2016-10-14 09:00:02 +0530

    build_server factory updated

--- bt/tests/factories/build_server_factory.py
+++ bt/tests/factories/build_server_factory.py
@@ -1,9 +1,15 @@
 import factory
 from .. import models
-from . import
+from . import BuildSiteFactory
 
 class BuildServerFactory(factory.Factory):
     class Meta:
         model = models.BuildServer
 
-    site_id =
+    site_id = factory.SubFactory(BuildSiteFactory)
+
+    server_name = "qpweb1"
+    instance_user = "questionpro"
+    batch = 1
+    start_app = 1
+    property_file = 'questionpro'
